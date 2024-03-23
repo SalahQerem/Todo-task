531c8a1aa15fd4b3a9b8273c5a27878820bb35fd
Salah AlDin <salahqerem@gmail.com>
2024-03-23 03:34:24 +0200

    Perform Create and delete oprations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,34 @@
+const addTodoForm = document.querySelector(".add-todo-form");
+const descriptionInput = document.querySelector("#todo-description");
+const addTodoBtn = document.querySelector(".add-todo-btn");
 const todosBody = document.querySelector("#todos-body");
 
+const getTodosFromLocalStorage = () => {
+  return JSON.parse(localStorage.getItem("todos"));
+};
+
+const setTodosToLocalStorage = (todos) => {
+  localStorage.setItem("todos", JSON.stringify(todos));
+  displayTodos();
+};
+
 const displayTodos = () => {
-  const todos = JSON.parse(localStorage.getItem("todos"));
+  const todos = getTodosFromLocalStorage();
   let renderedTodos = "";
-  todos.forEach((todo) => {
+  todos.forEach((todo, index) => {
     renderedTodos += `<tr>
-        <th scope="row">${todo.id}</th>
+        <th scope="row">${index + 1}</th>
         <td>${todo.todo}</td>
-        <td>${todo.completed ? "Completed" : "Pending"}</td>
         <td>${todo.userId}</td>
+        <td>${todo.completed ? "Completed" : "Pending"}</td>
         <td>
             <div class="btn-container">
-                <button class="btn btn-danger fw-semibold delete-btn">Delete</button>
-                <button class="btn btn-success fw-semibold confirm-btn">Done</button>
+                <button class="btn btn-danger fw-semibold delete-btn" onclick="deleteTodo(${
+                  todo.id
+                })">Delete</button>
+                <button class="btn btn-success fw-semibold confirm-btn" onclick="completeTodo(${
+                  todo.id
+                })">Done</button>
             </div>
         </td>
     </tr>`;
@@ -21,10 +37,43 @@ const displayTodos = () => {
 };
 
 const getTodos = async () => {
-  const res = await fetch("https://dummyjson.com/todos");
-  const { todos } = await res.json();
-  localStorage.setItem("todos", JSON.stringify(todos));
-  displayTodos();
+  if (!getTodosFromLocalStorage) {
+    const res = await fetch("https://dummyjson.com/todos");
+    const { todos } = await res.json();
+    setTodosToLocalStorage(todos);
+  } else {
+    displayTodos();
+  }
 };
 
 getTodos();
+
+const addTodo = (e) => {
+  e.preventDefault();
+  let todos = getTodosFromLocalStorage();
+  const todo = {
+    id: todos.length + 1,
+    todo: descriptionInput.value,
+    userId: 2,
+    completed: false,
+  };
+  todos = [todo, ...todos];
+  setTodosToLocalStorage(todos);
+  addTodoForm.reset();
+};
+
+const deleteTodo = (id) => {
+  let todos = getTodosFromLocalStorage();
+  const filteredTodos = todos.filter((todo) => todo.id !== id);
+  setTodosToLocalStorage(filteredTodos);
+};
+
+const completeTodo = (id) => {
+  let todos = getTodosFromLocalStorage();
+  todos.forEach((todo) => {
+    if (todo.id === id) todo.completed = true;
+  });
+  setTodosToLocalStorage(todos);
+};
+
+addTodoBtn.addEventListener("click", addTodo);
